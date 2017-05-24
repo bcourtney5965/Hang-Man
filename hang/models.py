c6410b3daf8ba0f525f10e33ed11ba0c6320c8ec
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-05-23 19:22:44 -0700

    Add database scheme

--- hang/models.py
+++ hang/models.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
 
-class Question(models.Model):
-    question_text = models.CharField(man_length=200)
-    pub_date = models.DateTimeField('date published')
+class Phrases(models.Models):
+    phrase = models.CharField(max_length=200)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Top_scores(models.Models):
+    name_text = models.ForeignKey(User)
+    phrase = models.ForeignKey(Phrases)
+    top_score = models.IntegerField(default=0)
